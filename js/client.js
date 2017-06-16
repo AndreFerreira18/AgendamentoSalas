a3f056cbcadc4e910ccef6a86cfeaecf3c3a7d89
AzureAD\MarianaRodrigues <mariana.rodrigues@>
2017-06-16 17:03:03 +0100

    Project struct organize

--- js/client.js
+++ js/client.js
@@ -32,3 +32,84 @@ $(document).ready(function(){
         }
     });
 });
+//Seletor de pisos
+document.getElementById("up").addEventListener("mouseover", mouseOver);
+document.getElementById("up").addEventListener("mouseout", mouseOut);
+document.getElementById("down").addEventListener("mouseover", mouseOver);
+document.getElementById("down").addEventListener("mouseout", mouseOut);
+var pisos = ["RC","1", "2", "3", "4", "5", "6", "7", "8"];
+var i;
+var nAndaresVisiveis = 4;
+
+function mouseOver(e) {
+    if (e.target.id == "up")
+        moveUp();
+    else if(e.target.id == "down")
+        moveDown();
+}
+function mouseOut(e){
+    if (e.target.id == "up")
+        document.getElementById("up").style.color = "black";
+    else if(e.target.id == "down")
+        document.getElementById("down").style.color = "black";
+}
+function writeOnScreen(min,max){
+    document.getElementById("selecionaPisos").innerHTML = " ";
+    for (i = min; i <= max; i++){
+        var create = document.createElement("p");
+        create.innerHTML = pisos[i];
+        create.classList.add('list-group-item');
+        create.id = 'piso-'+(i);
+        var element = document.getElementById("selecionaPisos");
+        element.prepend(create);
+    }
+}
+function moveUp(){
+    document.getElementById("down").style.display = 'block';
+    var min, max;
+    document.getElementById("up").style.color = "blue";
+    var aux_piso = document.getElementById("selecionaPisos").children[0];
+    var index = parseInt(aux_piso.id.split("-")[1]);
+    max = pisos.length-1 > index ? index+1 : index;
+    min = max-nAndaresVisiveis < 0 ? 0 : max-nAndaresVisiveis;
+    if (pisos.length-1 == index+1)
+        hideTopElement(min,max);
+    else
+        writeOnScreen(min,max);
+    function hideTopElement (min,max){
+        document.getElementById("up").style.display = 'none';
+        writeOnScreen (min-1,max);
+    }
+
+}
+function moveDown(){
+    document.getElementById("up").style.display = 'block';
+    var min, max;
+    document.getElementById("down").style.color = "blue";
+    var aux_piso = document.getElementById("selecionaPisos").lastElementChild;
+    var index = parseInt(aux_piso.id.split("-")[1]);
+    min = index-1 >= 0 ? index-1 : 0;
+    max= min+nAndaresVisiveis < pisos.length-1 ? min+nAndaresVisiveis : pisos.length-1;
+    if (index-1 == 0)
+        hideDownElement(min,max);
+    else
+        writeOnScreen(min,max);
+    function hideDownElement (min,max){
+        document.getElementById("down").style.display = 'none';
+        writeOnScreen (min,max+1);
+    }
+}
+
+
+if (nAndaresVisiveis > pisos.length-1)
+    nAndaresVisiveis = pisos.length-1;
+
+var min = 0;
+var max = nAndaresVisiveis-1;
+if (max-min == pisos.length-1){
+    document.getElementById("up").style.display ='none';
+    document.getElementById("down").style.display ='none';
+}
+writeOnScreen(min,max+1);
+nAndaresVisiveis--;
+document.getElementById("down").style.display = 'none';
