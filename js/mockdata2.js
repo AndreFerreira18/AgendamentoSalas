968b4ca2caf2124a8c88b33c296d3014475e4e85
NunoRodrigues15 <rodriguesnunorodrigues15@gmail.com>
2017-06-21 18:14:36 +0100

    Added Another Moked Data

---
+++ js/mockdata2.js
@@ -0,0 +1,378 @@
+var floors = {
+  "nAndares": 8,
+  "Andares": [{
+    "nome": "Res Do chão"
+  }, {
+    "nome": "1º Andar"
+  }, {
+    "nome": "2º Andar"
+  }, {
+    "nome": "3º Andar"
+  }, {
+    "nome": "4º Andar"
+  }, {
+    "nome": "5º Andar"
+  }, {
+    "nome": "6º Andar"
+  }, {
+    "nome": "7º Andar"
+  }]
+};
+
+var resources = {
+  "nPisos": 8,
+  "Piso": {
+    "Res Do chão": {
+      "nSalas": 4,
+      "Salas": [{
+          "NomeSala": "Sala 0.1",
+          "Recursos": {
+            "Projetor": "Sim",
+            "Material de Escritorio": "Sim",
+            "Flipchart": "Sim",
+            "Microfone": "Sim",
+            "Laser": "Sim"
+          }
+        },
+        {
+          "NomeSala": "Sala 0.2",
+          "Recursos": {
+            "Projetor": "Sim",
+            "Material de Escritorio": "Sim",
+            "Flipchart": "Sim",
+            "Microfone": "Sim",
+            "Laser": "Sim"
+          }
+        },
+        {
+          "NomeSala": "Sala 0.3",
+          "Recursos": {
+            "Projetor": "Sim",
+            "Material de Escritorio": "Sim",
+            "Flipchart": "Sim",
+            "Microfone": "Sim",
+            "Laser": "Sim"
+          }
+        },
+        {
+          "NomeSala": "Sala 0.4",
+          "Recursos": {
+            "Projetor": "Sim",
+            "Material de Escritorio": "Sim",
+            "Flipchart": "Sim",
+            "Microfone": "Sim",
+            "Laser": "Sim"
+          }
+        }
+      ]
+    },
+    "1º Andar": {
+      "nSalas": 4,
+      "Salas": [{
+          "NomeSala": "Sala 1.1",
+          "Recursos": {
+            "Projetor": "Sim",
+            "Material de Escritorio": "Sim",
+            "Flipchart": "Sim",
+            "Microfone": "Sim",
+            "Laser": "Sim"
+          }
+        },
+        {
+          "NomeSala": "Sala 1.2",
+          "Recursos": {
+            "Projetor": "Sim",
+            "Material de Escritorio": "Sim",
+            "Flipchart": "Sim",
+            "Microfone": "Sim",
+            "Laser": "Sim"
+          }
+        },
+        {
+          "NomeSala": "Sala 1.3",
+          "Recursos": {
+            "Projetor": "Sim",
+            "Material de Escritorio": "Sim",
+            "Flipchart": "Sim",
+            "Microfone": "Sim",
+            "Laser": "Sim"
+          }
+        },
+        {
+          "NomeSala": "Sala 1.4",
+          "Recursos": {
+            "Projetor": "Sim",
+            "Material de Escritorio": "Sim",
+            "Flipchart": "Sim",
+            "Microfone": "Sim",
+            "Laser": "Sim"
+          }
+        }
+      ]
+    },
+    "2º Andar": {
+      "nSalas": 4,
+      "Salas": [{
+          "NomeSala": "Sala 2.1",
+          "Recursos": {
+            "Projetor": "Sim",
+            "Material de Escritorio": "Sim",
+            "Flipchart": "Sim",
+            "Microfone": "Sim",
+            "Laser": "Sim"
+          }
+        },
+        {
+          "NomeSala": "Sala 2.2",
+          "Recursos": {
+            "Projetor": "Sim",
+            "Material de Escritorio": "Sim",
+            "Flipchart": "Sim",
+            "Microfone": "Sim",
+            "Laser": "Sim"
+          }
+        },
+        {
+          "NomeSala": "Sala 2.3",
+          "Recursos": {
+            "Projetor": "Sim",
+            "Material de Escritorio": "Sim",
+            "Flipchart": "Sim",
+            "Microfone": "Sim",
+            "Laser": "Sim"
+          }
+        },
+        {
+          "NomeSala": "Sala 2.4",
+          "Recursos": {
+            "Projetor": "Sim",
+            "Material de Escritorio": "Sim",
+            "Flipchart": "Sim",
+            "Microfone": "Sim",
+            "Laser": "Sim"
+          }
+        }
+      ]
+    },
+    "3º Andar": {
+      "nSalas": 4,
+      "Salas": [{
+          "NomeSala": "Sala 3.1",
+          "Recursos": {
+            "Projetor": "Sim",
+            "Material de Escritorio": "Sim",
+            "Flipchart": "Sim",
+            "Microfone": "Sim",
+            "Laser": "Sim"
+          }
+        },
+        {
+          "NomeSala": "Sala 3.2",
+          "Recursos": {
+            "Projetor": "Sim",
+            "Material de Escritorio": "Sim",
+            "Flipchart": "Sim",
+            "Microfone": "Sim",
+            "Laser": "Sim"
+          }
+        },
+        {
+          "NomeSala": "Sala 3.3",
+          "Recursos": {
+            "Projetor": "Sim",
+            "Material de Escritorio": "Sim",
+            "Flipchart": "Sim",
+            "Microfone": "Sim",
+            "Laser": "Sim"
+          }
+        },
+        {
+          "NomeSala": "Sala 3.4",
+          "Recursos": {
+            "Projetor": "Sim",
+            "Material de Escritorio": "Sim",
+            "Flipchart": "Sim",
+            "Microfone": "Sim",
+            "Laser": "Sim"
+          }
+        }
+      ]
+    },
+    "4º Andar": {
+      "nSalas": 4,
+      "Salas": [{
+          "NomeSala": "Sala 4.1",
+          "Recursos": {
+            "Projetor": "Sim",
+            "Material de Escritorio": "Sim",
+            "Flipchart": "Sim",
+            "Microfone": "Sim",
+            "Laser": "Sim"
+          }
+        },
+        {
+          "NomeSala": "Sala 4.2",
+          "Recursos": {
+            "Projetor": "Sim",
+            "Material de Escritorio": "Sim",
+            "Flipchart": "Sim",
+            "Microfone": "Sim",
+            "Laser": "Sim"
+          }
+        },
+        {
+          "NomeSala": "Sala 4.3",
+          "Recursos": {
+            "Projetor": "Sim",
+            "Material de Escritorio": "Sim",
+            "Flipchart": "Sim",
+            "Microfone": "Sim",
+            "Laser": "Sim"
+          }
+        },
+        {
+          "NomeSala": "Sala 4.4",
+          "Recursos": {
+            "Projetor": "Sim",
+            "Material de Escritorio": "Sim",
+            "Flipchart": "Sim",
+            "Microfone": "Sim",
+            "Laser": "Sim"
+          }
+        }
+      ]
+    },
+    "5º Andar": {
+      "nSalas": 4,
+      "Salas": [{
+          "NomeSala": "Sala 5.1",
+          "Recursos": {
+            "Projetor": "Sim",
+            "Material de Escritorio": "Sim",
+            "Flipchart": "Sim",
+            "Microfone": "Sim",
+            "Laser": "Sim"
+          }
+        },
+        {
+          "NomeSala": "Sala 5.2",
+          "Recursos": {
+            "Projetor": "Sim",
+            "Material de Escritorio": "Sim",
+            "Flipchart": "Sim",
+            "Microfone": "Sim",
+            "Laser": "Sim"
+          }
+        },
+        {
+          "NomeSala": "Sala 5.3",
+          "Recursos": {
+            "Projetor": "Sim",
+            "Material de Escritorio": "Sim",
+            "Flipchart": "Sim",
+            "Microfone": "Sim",
+            "Laser": "Sim"
+          }
+        },
+        {
+          "NomeSala": "Sala 5.4",
+          "Recursos": {
+            "Projetor": "Sim",
+            "Material de Escritorio": "Sim",
+            "Flipchart": "Sim",
+            "Microfone": "Sim",
+            "Laser": "Sim"
+          }
+        }
+      ]
+    },
+    "6º Andar": {
+      "nSalas": 4,
+      "Salas": [{
+          "NomeSala": "Sala 6.1",
+          "Recursos": {
+            "Projetor": "Sim",
+            "Material de Escritorio": "Sim",
+            "Flipchart": "Sim",
+            "Microfone": "Sim",
+            "Laser": "Sim"
+          }
+        },
+        {
+          "NomeSala": "Sala 6.2",
+          "Recursos": {
+            "Projetor": "Sim",
+            "Material de Escritorio": "Sim",
+            "Flipchart": "Sim",
+            "Microfone": "Sim",
+            "Laser": "Sim"
+          }
+        },
+        {
+          "NomeSala": "Sala 6.3",
+          "Recursos": {
+            "Projetor": "Sim",
+            "Material de Escritorio": "Sim",
+            "Flipchart": "Sim",
+            "Microfone": "Sim",
+            "Laser": "Sim"
+          }
+        },
+        {
+          "NomeSala": "Sala 6.4",
+          "Recursos": {
+            "Projetor": "Sim",
+            "Material de Escritorio": "Sim",
+            "Flipchart": "Sim",
+            "Microfone": "Sim",
+            "Laser": "Sim"
+          }
+        }
+      ]
+    },
+    "7º Andar": {
+      "nSalas": 4,
+      "Salas": [{
+          "NomeSala": "Sala 7.1",
+          "Recursos": {
+            "Projetor": "Sim",
+            "Material de Escritorio": "Sim",
+            "Flipchart": "Sim",
+            "Microfone": "Sim",
+            "Laser": "Sim"
+          }
+        },
+        {
+          "NomeSala": "Sala 7.2",
+          "Recursos": {
+            "Projetor": "Sim",
+            "Material de Escritorio": "Sim",
+            "Flipchart": "Sims",
+            "Microfone": "Sim",
+            "Laser": "Sim"
+          }
+        },
+        {
+          "NomeSala": "Sala 7.3",
+          "Recursos": {
+            "Projetor": "Sim",
+            "Material de Escritorio": "Sim",
+            "Flipchart": "Sim",
+            "Microfone": "Sim",
+            "Laser": "Sim"
+          }
+        },
+        {
+          "NomeSala": "Sala 7.4",
+          "Recursos": {
+            "Projetor": "Sim",
+            "Material de Escritorio": "Sim",
+            "Flipchart": "Sim",
+            "Microfone": "Sim",
+            "Laser": "Sim"
+          }
+        }
+      ]
+    }
+  }
+};
